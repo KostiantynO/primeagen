23207d5c9782a3758d00fc80592bb7eb51b07710
Kostiantyn Ochenash <Ochenashk@gmail.com>
2024-01-30 10:34:35 +0200

    feat: adds CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,4 +1,23 @@
 export const compareBinaryTrees = (
   a: BinaryNode<number> | null,
   b: BinaryNode<number> | null
-): boolean => {};
+): boolean => {
+  // structural check
+  if (a === null && b === null) {
+    return true;
+  }
+
+  // structural check
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // value check
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return (
+    compareBinaryTrees(a.left, b.left) && compareBinaryTrees(a.right, b.right)
+  );
+};
