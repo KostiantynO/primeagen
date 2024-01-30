1b8fb3128611ea7c0dffdf49ed8dea0eea5150c6
Kostiantyn Ochenash <Ochenashk@gmail.com>
2024-01-30 09:40:12 +0200

    feat: adds BTBFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,1 +1,23 @@
-export const btbfs = (head: BinaryNode<number>, needle: number): boolean => {};
+export const btbfs = (head: BinaryNode<number>, needle: number): boolean => {
+  const q: (BinaryNode<number> | null)[] = [head];
+
+  while (q.length) {
+    const curr = q.shift();
+
+    if (!curr) {
+      continue;
+    }
+
+    const { value, left, right } = curr;
+
+    // search
+    if (value === needle) {
+      return true;
+    }
+
+    q.push(left);
+    q.push(right);
+  }
+
+  return false;
+};
